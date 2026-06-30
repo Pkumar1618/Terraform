d6fc99a2c4c556caa97cbae885f95d303b004c6c
pkumar1618 <prashanthch943@gmail.com>
2026-06-30 15:11:29 +0530

    merging the tags usng functions

--- countloop/ec2.tf
+++ countloop/ec2.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "backend" {
-    count = 3 
+    count = 3 # it means create the 3 instances.
     ami      = "ami-0220d79f3f480ecf5"
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
 
     tags = {
-        Name = var.instance_names[count.index]
+        Name = var.instance_names[count.index] # In Terraform, count.index is used to identify the index number of the current resource instance when you create multiple resources using the count meta-argument.
     }
 }
 
