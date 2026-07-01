6598392a1b43a10c04872050b5a3f1c1c2892c14
pkumar1618 <prashanthch943@gmail.com>
2026-07-01 10:59:44 +0530

    using terraform create the ec2 instance with route 53

--- expense/outputs.tf
+++ expense/outputs.tf
@@ -1,3 +1,6 @@
+# use output.instance_info
+# output.instance_info == aws_instance.expense
+
 output "instances_info" {
   value = aws_instance.expense
 }
--- expense/r53.tf
+++ expense/r53.tf
@@ -7,6 +7,6 @@ resource "aws_route53_record" "expense" {
   allow_overwrite = true
 
   records = [
-    aws_instance.expense[count.index].private_ip
-  ]
+    aws_instance.expense[count.index].private_ip 
+  ] # this is list type that is reson we have given to count.index.
 }
