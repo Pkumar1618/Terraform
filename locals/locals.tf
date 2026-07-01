b3cae16edfd7cd222fea4fc84e6bc77193ca9f38
pkumar1618 <prashanthch943@gmail.com>
2026-07-01 23:56:21 +0530

    created instances using locals

---
+++ locals/locals.tf
@@ -0,0 +1,5 @@
+locals {
+  domain_name = "daws81.online"
+  zone_id     = "Z02579582QF6L8MOAFJQP"
+  # count.index not works in locals that's why here we couldn't add instances.
+}
---
+++ locals/variables.tf
@@ -0,0 +1,12 @@
+variable "instance_names" {
+  type    = list(string)
+  default = ["mysql", "backend", "frontend"]
+}
+
+# variable "domain_name" {
+#     default = "daws81.online"
+# }
+
+# variable "zone_id" {
+#     default = "Z02579582QF6L8MOAFJQP"
+# }
